41ca81fd19b520e0301966deb402b6f7f96589de
bengris32 <bengris32@protonmail.ch>
2023-07-07 23:10:44 +0700

    rosemary: Build MediaTek boot control HAL
    Change-Id: I4f453cfa2c44dcbbd7c611ec55624aff3d7e3d5d
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -15,6 +15,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 # A/B
 PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-mtkimpl \
+    android.hardware.boot@1.2-mtkimpl.recovery \
+    android.hardware.boot@1.2-service
+
+PRODUCT_PACKAGES += \
     update_engine \
     update_engine_sideload \
     update_verifier
