84cf3fda24eb4ae0e9eb9c1efc7aaede3f00b708
bengris32 <bengris32@protonmail.ch>
2023-07-20 14:56:18 +0100

    rosemary: Build `android.hidl.allocator@1.0` vendor variant
    * Dependency of PQ HAL.
    
    Change-Id: I6050c49755c93ac24f62ca7ea0f0a1ea96e477cd
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -194,7 +194,8 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     libhidltransport.vendor \
-    libhwbinder.vendor
+    libhwbinder.vendor \
+    android.hidl.allocator@1.0.vendor
 
 # ConsumerIr
 PRODUCT_PACKAGES += \
