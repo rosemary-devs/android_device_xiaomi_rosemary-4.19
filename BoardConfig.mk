d586d027bd620c92c9722fb4ac2b3171f0bf2c9e
Aayush Gupta <aayushgupta219@gmail.com>
2023-08-05 12:34:33 +0100

    rosemary: Inherit sepolicy for mediatek devices
    There are lot of denials which are not device-specific. Henceforth,
    move such denials to a seperate sepolicy repository and inherit from it.
    
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I95c1f35006d49b797c617c5fea5ff27e5f98d45e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -162,6 +162,9 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # SPL
 VENDOR_SECURITY_PATCH := 2023-10-01
 
+# SELinux
+include device/mediatek/sepolicy_vndr/SEPolicy.mk
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
