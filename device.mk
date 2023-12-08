1f2427cbfb39dd60ac4b2881e27eb4dd0f92c973
dread <dreadcreator5@gmail.com>
2023-12-08 17:55:44 +0530

    rosemary: Import runtime resource overlays
    Signed-off-by: dread <dreadcreator5@gmail.com>

--- device.mk
+++ device.mk
@@ -260,6 +260,15 @@ PRODUCT_PACKAGES += \
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+# Overlays
+PRODUCT_PACKAGES += \
+    CarrierConfigOverlayRosemary \
+    FrameworksResOverlayRosemary \
+    SystemUIOverlayRosemary \
+    TelephonyOverlayRosemary \
+    TetheringConfigOverlayRosemary \
+    WifiOverlayRosemary
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
