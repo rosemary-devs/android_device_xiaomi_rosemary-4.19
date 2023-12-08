c3889be83822314b717110ba0094b0d8c7b0c77f
dread <dreadcreator5@gmail.com>
2023-12-08 17:17:39 +0530

    rosemary: configure GNSS stack
    Signed-off-by: dread <dreadcreator5@gmail.com>

--- device.mk
+++ device.mk
@@ -142,6 +142,17 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-service \
     android.hardware.gatekeeper@1.0-impl
 
+# GNSS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.0.vendor \
+    android.hardware.gnss@1.1.vendor \
+    android.hardware.gnss@2.0.vendor \
+    android.hardware.gnss@2.1.vendor \
+    android.hardware.gnss.measurement_corrections@1.0.vendor \
+    android.hardware.gnss.measurement_corrections@1.1.vendor \
+    android.hardware.gnss.visibility_control@1.0.vendor \
+    android.hardware.gnss-service.mediatek
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0.vendor \
