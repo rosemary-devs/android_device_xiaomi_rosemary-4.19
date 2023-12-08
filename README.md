bfb0bc376e449bd36db5976ec5e3c2816fd40745
dread <dreadcreator5@gmail.com>
2023-12-08 10:14:13 +0530

    rosemary: Initial Skeleton tree
    Signed-off-by: dread <dreadcreator5@gmail.com>

---
+++ README.md
@@ -0,0 +1,9 @@
+# Android Device tree for the Redmi Note 10S
+
+```
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ lineage_rosemary.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from device makefile.
+$(call inherit-product, device/xiaomi/rosemary/device.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_rosemary
+PRODUCT_DEVICE := rosemary
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := Redmi Note 10S
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="rosemary-user 12 SP1A.210812.016 V13.0.6.0.SKLMIXM release-keys"
+
+BUILD_FINGERPRINT := Redmi/rosemary_global/rosemary:12/SP1A.210812.016/V13.0.6.0.SKLMIXM:user/release-keys
