3e9225cd063a95dac0ea76ae1013bb0de09c3118
dread <dreadcreator5@gmail.com>
2023-12-08 15:40:59 +0530

    rosemary: Configure Thermal Stack
    Signed-off-by: dread <dreadcreator5@gmail.com>

--- device.mk
+++ device.mk
@@ -280,6 +280,12 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/mediatek \
     hardware/xiaomi
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0.vendor \
+    android.hardware.thermal@2.0.vendor
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0.vendor \
