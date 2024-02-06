75f693f625a0867e204e8f14cf2e05c2667a5ba9
andreock <vivanco11.ac@gmail.com>
2024-02-06 09:02:48 +0530

    rosemary: Remove AudioFX
    Signed-off-by: dread <dreadcreator5@gmail.com>

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -3,8 +3,10 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
+
 LOCAL_OVERRIDES_PACKAGES := \
 		  MatLog \
+		  AudioFX
 
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := platform
