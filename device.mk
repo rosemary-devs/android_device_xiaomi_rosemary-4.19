60571322078094a3ca11918a4fd5942545931bf1
dread <dreadcreator5@gmail.com>
2024-02-27 21:40:27 +0530

    rosemary: checkout configs from `14.0.10.0.TFFMIXM`
    Signed-off-by: dread <dreadcreator5@gmail.com>

--- device.mk
+++ device.mk
@@ -435,11 +435,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/perf,$(TARGET_COPY_OUT_VENDOR)/etc)
 
-# Cgroup
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
-    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
-
 # Properties
 include $(LOCAL_PATH)/vendor_logtag.mk
 
